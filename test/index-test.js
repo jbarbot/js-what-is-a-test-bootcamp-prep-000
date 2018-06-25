9a35cd7e6e2320b47dc5609a1c73a798e3ef55d2
Jacob Barbot <barbotjacob@gmail.com>
2018-06-25 14:32:25 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,17 +2,17 @@ var index = require("../index.js")
 
 
 
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
+describe('Name', () => {
+  it('returns "Susan"', () => {
+     expect(index.name).toEqual('Susan')
+   })
+ })
 
 
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
-    })
+describe('Height', () => {
+   it('is less than 40', () => {
+     expect(index.height).toBeLessThan(40)
+     })
   })
 
   describe('Message', () => {
